80da6c2e218f944481ff0d4e755d006b39797155
addpadding <tiktokaddison20@gmail.com>
2023-03-06 22:18:59 +0300

    lesson-60 = War-Game - Part 3 => End

--- js/main.js
+++ js/main.js
@@ -9,7 +9,30 @@ Character.prototype.attack = function (opponent) {
     console.log(opponent.health);
 };
 
+Character.prototype.status = function () {
+    console.log(`Name: ${this.name}`);
+    console.log(`Strenght: ${this.strenght}`);
+    console.log(`Health: ${this.health}`);
+}
+
+Character.prototype.makeHealsh = function () {
+
+    if (this.health < 100) {
+        this.health += 10
+    }
+
+    if (this.health > 100) {
+        this.health = 100
+    }
+
+}
+
+
 let naruto = new Character("naruto", 10, 100);
 let sasuke = new Character("sasuke", 5, 100);
 
-sasuke.attack(naruto);
+sasuke.attack(naruto)
+
+naruto.status()
+naruto.makeHealsh()
+naruto.status()
