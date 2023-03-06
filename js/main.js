81983195ec2d50dc3d89e3ddb42d977ee99482e0
addpadding <tiktokaddison20@gmail.com>
2023-03-07 00:36:44 +0300

    lesson-61 = War-Game - Part 4 => End

--- js/main.js
+++ js/main.js
@@ -2,10 +2,16 @@ function Character(name, strenght, health) {
     this.name = name;
     this.strenght = strenght;
     this.health = health;
+
+
+    this.attackBtn = document.querySelector(`#${this.name}-attack`)
+    this.healthkBtn = document.querySelector(`#${this.name}-make-health`)
+    this.progress = document.querySelector(`.${this.name}-health span`)
 }
 
 Character.prototype.attack = function (opponent) {
     opponent.health -= this.strenght;
+    opponent.progress.style.width = `${opponent.health}%`
     console.log(opponent.health);
 };
 
@@ -19,6 +25,7 @@ Character.prototype.makeHealsh = function () {
 
     if (this.health < 100) {
         this.health += 10
+        this.progress.style.width = `${this.health}%`
     }
 
     if (this.health > 100) {
@@ -31,8 +38,24 @@ Character.prototype.makeHealsh = function () {
 let naruto = new Character("naruto", 10, 100);
 let sasuke = new Character("sasuke", 5, 100);
 
-sasuke.attack(naruto)
+console.log(sasuke)
 
-naruto.status()
-naruto.makeHealsh()
-naruto.status()
+
+naruto.attackBtn.addEventListener("click", function () {
+    naruto.attack(sasuke)
+})
+
+sasuke.attackBtn.addEventListener("click", function () {
+    sasuke.attack(naruto)
+})
+
+
+
+
+naruto.healthkBtn.addEventListener("click", function () {
+    naruto.makeHealsh()
+})
+
+sasuke.healthkBtn.addEventListener("click", function () {
+    sasuke.makeHealsh()
+})
